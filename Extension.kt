2ec01b878ffba003f3e2dfc5ad4fc9fc2396e5f7
Egor Prokhorov <foxiario@gmail.com>
2022-10-09 18:42:00 +0300

    delet

--- Extension.kt
+++
@@ -1,5 +0,0 @@
-fun Int.convert(radix: Int) : Int
-{
-    val temp = this.toString(radix)
-    return temp.toInt()
-}
--- IO.kt
+++
@@ -1,4 +0,0 @@
-interface IO {
-    fun read() : String
-    fun write(data : String)
-}
